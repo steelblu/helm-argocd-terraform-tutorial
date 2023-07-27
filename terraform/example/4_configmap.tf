ac4197e3009e1060f55055b41184817b09304e32
gorani <akfmdl@naver.com>
2023-07-27 14:16:02 +0000

    feat: modify configmap, deploy for auto rollout

--- terraform/example/4_configmap.tf
+++ terraform/example/4_configmap.tf
@@ -1,15 +1,16 @@
+locals {
+  configmap_data = templatefile("${path.module}/index.html.tpl", {
+    content = var.content
+  })
+}
+
 resource "kubernetes_config_map" "this" {
   metadata {
     name      = "nginx-html"
     namespace = kubernetes_namespace.this.metadata[0].name
   }
+
   data = {
-    "index.html" = <<EOF
-      <html>
-      <body>
-      <h1>${var.content}</h1>
-      </body>
-      </html>
-    EOF
+    "index.html" = local.configmap_data
   }
 }
--- terraform/example/5_deployment.tf
+++ terraform/example/5_deployment.tf
@@ -3,35 +3,48 @@ resource "kubernetes_deployment" "this" {
     name      = "nginx-deployment"
     namespace = kubernetes_namespace.this.metadata[0].name
   }
+
   spec {
     replicas = 1
+
     selector {
       match_labels = {
         app = var.label
       }
     }
+
     template {
       metadata {
         labels = {
           app = var.label
         }
       }
+
       spec {
         container {
           image = "nginx"
-          name = "nginx"
+          name  = "nginx"
+
+          env {
+            name  = "CONFIG_HASH"
+            value = sha256(local.configmap_data)
+          }
+
           port {
             container_port = 80
           }
+
           volume_mount {
             mount_path = "/usr/share/nginx/html"
-            name = "html"
+            name       = "html"
           }
         }
+
         volume {
           name = "html"
+
           config_map {
-            name = "nginx-html"
+            name = kubernetes_config_map.this.metadata[0].name
           }
         }
       }
